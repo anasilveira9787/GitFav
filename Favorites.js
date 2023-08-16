19b08c4b7db0e711895005abd0ca316c9993577a
Ana Silveira <ana.ccord@gmail.com>
2023-08-16 19:24:04 -0300

    first commit

---
+++ Favorites.js
@@ -0,0 +1,194 @@
+import { GithubUser } from "./GithubUser.js"
+
+
+// classe que vai conter a lógica dos dados
+// como os dados serão estruturados
+
+export class Favorites {
+
+    constructor(root) {
+        this.root = document.querySelector(root)
+        this.load()
+        
+    }
+
+
+    load() {
+
+        this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+
+        }
+    
+    save() {
+        localStorage.setItem('@github-favorites:', JSON.stringify(this.entries))
+    }
+
+
+
+
+    async add(username) {
+        try {
+
+            const userExists = this.entries.find(entry => entry.login === username)
+
+            if(userExists){
+                throw new Error('Usuário já cadastrado')
+            }
+
+
+
+            const user = await GithubUser.search(username)
+
+            if(user.login === undefined) {
+                throw new Error('Usuário não encontrado!')
+            }
+
+            this.entries = [user, ...this.entries]
+            this.update()
+            this.save()
+
+
+            } catch(error) {
+                alert(error.message)
+            }        
+
+    }
+
+
+
+    
+    delete(user) {
+
+        const filteredEntries = this.entries.filter(entry => entry.login !== user.login)
+
+        this.entries = filteredEntries
+
+        this.update()
+        this.save()
+
+        if(filteredEntries.length ===0) {
+            const row = this.createEmptyRow()
+            this.tbody.append(row)
+        }
+
+
+    }
+
+
+
+}
+
+
+
+// classe que vai criar a visualização e eventos do HTML
+
+export class FavoritesView extends Favorites {
+    constructor(root) {
+        super(root)
+
+        this.tbody = this.root.querySelector('table tbody')
+
+        this.update()
+        this.onadd()
+    }
+
+
+
+    onadd() {
+        const addButton = this.root.querySelector('.search button')
+        addButton.onclick = () => {
+            const { value } = this.root.querySelector('.search input')
+
+            this.add(value)
+
+        }
+    }
+    
+
+    update() {
+
+        this.removeAllTr() 
+      
+
+        this.entries.forEach(user => {
+            const row = this.createRow()
+            
+            row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+            row.querySelector('.user img').alt = `Imagem de ${user.name}`
+            row.querySelector('.user a').href = `https://github.com/${user.login}`
+            row.querySelector('.user p').textContent = user.name
+            row.querySelector('.user span').textContent = user.login
+            row.querySelector('.repositories').textContent = user.public_repos
+            row.querySelector('.followers').textContent = user.followers
+
+            row.querySelector('.remove').onclick = () => {
+                const isOk = confirm('Tem certeza que deseja deletar essa linha?')
+
+                if(isOk) {
+                    this.delete(user)
+                }
+            }
+
+            this.tbody.append(row)
+        })
+        
+        
+        
+    }
+
+    createRow() {
+        const tr = document.createElement('tr')
+        tr.innerHTML = `
+        <td class="user">
+           <img src="" alt="Imagem de maykbrito">
+           <a href="" target="_blank">
+             <p></p>
+             <span></span>
+           </a>
+         </td>
+         <td class="repositories">
+           
+         </td>
+         <td class="followers">
+         </td>
+         <td>
+           <span class="remove">Remover</span>
+         </td>
+        `
+
+        return tr
+
+        }
+
+    createEmptyRow() {
+            const tr = document.createElement('tr')
+            tr.innerHTML = `
+            <td class="emptyRow" colspan="4">
+            <div>
+            <img src="./assets/scariedStar.svg" alt="">
+            <span>Nenhum favorito ainda</span></div>
+        
+            </td>
+            `
+    
+            return tr
+    
+            }
+    
+
+    removeAllTr() {
+        const line = this.tbody.querySelectorAll('tr')
+
+        if(line.length ===0) {
+            const row = this.createEmptyRow()
+            this.tbody.append(row)
+        }         
+
+        
+
+        line.forEach((tr) => {
+            tr.remove()
+        });
+    }
+}
+
